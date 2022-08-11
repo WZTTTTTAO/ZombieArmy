79147b575e6da08d71d8448fc0944e4d3d8e4cb1
fourstarrrr <2040018270@qq.com>
2022-08-11 10:53:04 +0800

    fix Marsh Bug

---
+++ Assets/Scripts/Character/MarshCharacter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace ZombieArmy.Character
+{
+
+
+    public class MarshCharacter : MonoBehaviour
+    {
+        private NavMeshAgent nav;
+        public float marshSlowd = 0.2f;
+        private float Speed;
+        void Start()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            Speed = nav.speed;
+        }
+
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Marsh")
+            {
+                nav.speed = Speed * marshSlowd;
+            }
+        }
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.tag == "Marsh")
+            {
+                nav.speed = Speed;
+            }
+        }
+    }
+}
