7c4a4c9ac9467f2d2672be6922145876a7e008e4
kfeng3 <59978455+kfeng3@users.noreply.github.com>
2022-08-20 00:03:11 -0400

    NewZombieArtAssetsAndLevelEdit
    two character art assets along with some level adjustments. HealthBar bug found

---
+++ Assets/Scripts/Character/BillboardEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardEffect : MonoBehaviour
+{
+   private Camera theCam;
+   public bool useStaticBillboard;
+
+   void Start()
+   {
+        theCam = Camera.main;
+   }
+
+   void LateUpdate()
+   {
+        if (!useStaticBillboard)
+        {
+            transform.LookAt(theCam.transform);
+        }
+        else
+        {
+            transform.rotation = theCam.transform.rotation;
+        }
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x*180f, 0f, 0f);
+   }
+}
