fe712502cfaf1e0f6c3640a34dc845050f942946
fourstarrrr <2040018270@qq.com>
2022-08-25 22:24:12 +0800

    wall fall

---
+++ Assets/Scripts/Trap/Wall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ZombieArmy.Character
+{
+    public class Wall : BaseStatus
+    {
+        public BaseStatus baseStatus;
+        private float studentHp;
+        [SerializeField] private float nowHp;
+        private Animator anim;
+
+        private void Start()
+        {
+            studentHp = GetComponent<StudentStatus>().characterStatusInfo.MaxHealth;
+            anim = GetComponent<Animator>();
+        }
+        void Update()
+        {
+            nowHp = currentHealth;
+            if(studentHp>nowHp)
+            {
+                anim.SetBool("Fall", true);
+            }
+        }
+    }
+}
